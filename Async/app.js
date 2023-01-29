9ea600b792d07874e8690cc381523aef70e9dca2
Stevie <steviescripts52@gmail.com>
2023-01-28 21:56:50 -0800

    'part 2 finished with refactoring'

--- Async/app.js
+++ Async/app.js
@@ -49,3 +49,37 @@ async function getCard() {
     let card = res.data.cards[0]
     console.log(`${card.value} of ${card.suit}`)
 }
+getCard()
+
+// 2. Get one card from a new shuffled deck and then draw another card from the same deck
+
+async function getCard() {
+    let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    let deckId = res.data.deck_id
+    let res2 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    let card = res2.data.cards[0]
+    let card2 = res2.data.cards[1]
+    console.log(`${card.value} of ${card.suit}`)
+    console.log(`${card2.value} of ${card2.suit}`)
+}
+getCard()
+
+// 3. On button click, get a new shuffled deck, and then display a new card each time the button is clicked
+
+const btn = document.querySelector('#deck-btn')
+const deck = document.querySelector('#deck')
+
+
+btn.addEventListener('click', async function() {
+    let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    let deckId = res.data.deck_id
+    let res2 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    let card = res2.data.cards[0]
+    let img = document.createElement('img')
+    img.src = card.image
+    deck.append(img)
+
+    console.log(`${card.value} of ${card.suit}`)
+})
+
+getCard()
